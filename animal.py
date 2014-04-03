62f2713984c9f6855dbadebb885b32c49dd9fbd5
Maria Krusteva <mimito_68@abv.bg>
2014-04-03 20:29:13 +0300

    Adding basics on animal class

---
+++ animal.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+
+def cur():
+    conn = sqlite3.connect("animals.db")
+    return conn.cursor()
+
+
+class Animal():
+
+    """docstring for Animal"""
+
+    def __init__(self, species, age, name, gender, weight):
+        self.species = species
+        self.age = age
+        self.name = name
+        self.gender = gender
+        self.weight = weight
+
+    def grow(self):
+        '''
+        The animal grows with weight_age_ratio
+        for 1 month
+        '''
+        query = '''SELECT average_weight, weight_age_ratio FROM animals 
+                   WHERE species = ?'''
+        cursor = cur()
+        result = cursor.execute(query, (self.species, )).fetchone()
+        average = result[0]
+        ratio = result[1]
+
+        if self.weight < average:
+            self.weight += ratio
+
+
+def main():
+    a = Animal("tiger", 20, "TIGYR", "male", 100)
+    print(a.weight)
+    a.grow()
+    print(a.weight)
+
+
+if __name__ == '__main__':
+    main()
